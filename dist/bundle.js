0601040e29a2eb69005c83e0bbe984f379ce6496
Jonathan <jonathanjohnson023@gmail.com>
2019-11-13 21:54:14 -1000

    it work

--- dist/bundle.js
+++ dist/bundle.js
@@ -114,18 +114,17 @@ class Character {
     return Math.abs(n % 2) == 1;
   }
 
-  draw(ctx){
-    debugger
-    ctx.drawImage(this.sprite, 0, 0, 64, 64, this.ctx.width * 0.75, this.ctx.height * this.heightFloat + 150 , 125, 125 )
+  draw(){
+    this.ctx.drawImage(this.sprite, 0, 0, 64, 64, this.ctx.canvas.width * 0.85, this.ctx.canvas.height * this.heightFloat + this.ctx.canvas.height * 0.3 , 125, 125 )
   }
 
   walkForward(frame){
     this.frame = frame
     if(this.frame >= 10) this.frame = 10
     if(this.isOdd(this.frame)){
-      this.ctx.drawImage(this.sprite, 64, 0, 64, 64, this.ctx.width * 0.75 - parseInt(this.frame + "0"), this.ctx.height * this.heightFloat + 150 , 125, 125 )
+      this.ctx.drawImage(this.sprite, 64, 0, 64, 64, this.ctx.canvas.width * 0.85 - parseInt(this.frame + "0"), this.ctx.canvas.height * this.heightFloat + this.ctx.canvas.height * 0.3 , 125, 125 )
     }else{
-      this.ctx.drawImage(this.sprite, 0, 0, 64, 64, this.ctx.width * 0.75 - parseInt(this.frame + "0"), this.ctx.height * this.heightFloat + 150 , 125, 125 )
+      this.ctx.drawImage(this.sprite, 0, 0, 64, 64, this.ctx.canvas.width * 0.85 - parseInt(this.frame + "0"), this.ctx.canvas.height * this.heightFloat + this.ctx.canvas.height * 0.3 , 125, 125 )
     }
   }
 
